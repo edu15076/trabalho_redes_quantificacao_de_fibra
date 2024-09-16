eed798f7ddeb1cb52a373c61727b892be4437f14
Eduardo Costa de Souza <eduardbh2014@gmail.com>
2024-09-16 02:39:56 -0300

    Quantificadores semi-funcionais e melhorias na tela

--- fibra/forms_quantificacao.py
+++ fibra/forms_quantificacao.py
@@ -1,37 +1,106 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Field
+from crispy_forms.layout import Layout, Field, HTML
+from django.contrib.admin.utils import help_text_for_field
 
 
 class FormConfig(forms.Form):
-    modo_fibra_seq_primaria = forms.CharField(label="Modo da Fibra na SEQPrincipal")
-    nucleo_fibra_seq_primaria = forms.CharField(label="Núcleo da Fibra na SEQPrincipal")
-    indice_fibra_seq_primaria = forms.CharField(label="Índice da Fibra na SEQPrincipal")
-    categoria_fibra_seq_primaria = forms.CharField(label="Categoria da Fibra na SEQPrincipal")
+    backbone_primario = forms.BooleanField(
+        label="Quantificar Backbone Primário",
+        required=False,
+        help_text='Implica em quantificar SEQ primária e secundária'
+    )
+    backbone_secundario = forms.BooleanField(
+        label="Backbone Secundário",
+        required=False,
+        help_text='Implica em quantificar SEQ secundária e SETs'
+    )
 
-    modo_fibra_seq_secundaria = forms.CharField(label="Modo da Fibra na SEQSecundaria")
-    nucleo_fibra_seq_secundaria = forms.CharField(label="Núcleo da Fibra na SEQSecundaria")
-    indice_fibra_seq_secundaria = forms.CharField(label="Índice da Fibra na SEQSecundaria")
-    categoria_fibra_seq_secundaria = forms.CharField(label="Categoria da Fibra na SEQSecundaria")
+    modo_fibra_seq_primaria = forms.CharField(label="Modo da Fibra na SEQ primária", required=False,
+                                              help_text="SM ou MM")
+    nucleo_fibra_seq_primaria = forms.CharField(label="Núcleo da Fibra na SEQ primária", required=False,
+                                                help_text="Valor inteiro, 9, 50, ...")
+    indice_fibra_seq_primaria = forms.CharField(label="Índice da Fibra na SEQ primária", required=False,
+                                                help_text="IG, ID, ...")
+    categoria_fibra_seq_primaria = forms.CharField(label="Categoria da Fibra na SEQ primária", required=False,
+                                                   help_text='Loose, tight buffer, ...')
 
-    modo_fibra_set = forms.CharField(label="Modo da Fibra na SET")
-    nucleo_fibra_set = forms.CharField(label="Núcleo da Fibra na SET")
-    indice_fibra_set = forms.CharField(label="Índice da Fibra na SET")
-    categoria_fibra_set = forms.CharField(label="Categoria da Fibra na SET")
+    modo_fibra_seq_secundaria = forms.CharField(label="Modo da Fibra nas SEQs secundárias",
+                                              help_text="SM ou MM")
+    nucleo_fibra_seq_secundaria = forms.CharField(label="Núcleo da Fibra nas SEQs secundárias",
+                                                help_text="Valor inteiro, 9, 50, ...")
+    indice_fibra_seq_secundaria = forms.CharField(label="Índice da Fibra nas SEQs secundárias",
+                                                help_text="IG, ID, ...")
+    categoria_fibra_seq_secundaria = forms.CharField(label="Categoria da Fibra nas SEQs secundárias",
+                                                   help_text='Loose, tight buffer, ...')
 
-    backbone_primario = forms.BooleanField(label="Backbone Primário", required=False)
-    backbone_secundario = forms.BooleanField(label="Backbone Secundário", required=False)
+    modo_fibra_set = forms.CharField(label="Modo da Fibra nas SETs", required=False,
+                                              help_text="SM ou MM")
+    nucleo_fibra_set = forms.CharField(label="Núcleo da Fibra nas SETs", required=False,
+                                                help_text="Valor inteiro, 9, 50, ...")
+    indice_fibra_set = forms.CharField(label="Índice da Fibra nas SETs", required=False,
+                                                help_text="IG, ID, ...")
+    categoria_fibra_set = forms.CharField(label="Categoria da Fibra nas SETs", required=False,
+                                                   help_text='Loose, tight buffer, ...')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            HTML('<h2>Backbones à serem quantificados</h2>'),
+            Field('backbone_primario'),
+            Field('backbone_secundario'),
+            HTML('<p class="form-text">Ao menos um deve ser selecionado, se ambos forem'
+                 ' serão quantificadas a SEQ primária, SEQs secundárias e SETs</p>'),
+            HTML('<h2>Dados da fibra chegando na SEQ primária</h2>'),
+            Field('modo_fibra_seq_primaria'),
+            Field('nucleo_fibra_seq_primaria'),
+            Field('indice_fibra_seq_primaria'),
+            Field('categoria_fibra_seq_primaria'),
+            HTML('<p class="form-text">Podem, preferencialmente devem, estar vazios no '
+                 'caso de não estar quantificando Backbones Primário</p>'),
+            HTML('<h2>Dados das fibras chegando nas SEQs secundárias</h2>'),
+            Field('modo_fibra_seq_secundaria'),
+            Field('nucleo_fibra_seq_secundaria'),
+            Field('indice_fibra_seq_secundaria'),
+            Field('categoria_fibra_seq_secundaria'),
+            HTML('<p class="form-text">Deve ser preenchido em qualquer tipo de '
+                 'quantificação</p>'),
+            HTML('<p class="form-text">Essa configuração é aplicada para todas as '
+                 'fibras em SEQs secundárias</p>'),
+            HTML('<h2>Dados das fibras chegando nas SETs</h2>'),
+            Field('modo_fibra_set'),
+            Field('nucleo_fibra_set'),
+            Field('indice_fibra_set'),
+            Field('categoria_fibra_set'),
+            HTML('<p class="form-text">Podem, preferencialmente devem, estar vazios no '
+                 'caso de não estar especificando Backbones Secundários</p>'),
+            HTML('<p class="form-text">Essa configuração é aplicada para todas as '
+                 'fibras em SETs</p>'),
+        )
 
 
 class FormSEQPrimaria(forms.Form):
-    medida_basica_seq_primaria = forms.FloatField(label="Medida Básica SEQPrimaria", required=False)
-    disciplinas_seq_primaria = forms.IntegerField(label="Disciplinas SEQPrimaria", required=False)
+    medida_basica_seq_primaria = forms.FloatField(label="Distância média das SEQs secundárias à SEQ primária", required=False)
+    disciplinas_seq_primaria = forms.IntegerField(label="Disciplinas chegando na SEQ primária", required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            HTML('<h2>Dados SEQ primária</h2>'),
+            Field('medida_basica_seq_primaria'),
+            Field('disciplinas_seq_primaria'),
+            HTML('<p class="form-text">Só devem ser preenchido no caso de estar especificando o backbone principal</p>'),
+        )
 
 
 class FormSEQSecundaria(forms.Form):
     andar = forms.CharField(label="Andar")
-    medida_basica_seq_secundaria = forms.FloatField(label="Medida Básica SEQSecundaria", required=False)
-    disciplinas_seq_secundaria = forms.IntegerField(label="Disciplinas SEQSecundaria", required=False)
+    medida_basica_seq_secundaria = forms.FloatField(label="Pé direito SEQ secundária")
+    disciplinas_seq_secundaria = forms.IntegerField(label="Disciplinas chegando na SEQ secundária")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -41,6 +110,9 @@ class FormSEQSecundaria(forms.Form):
             Field('andar', css_class='andar_seq_secundaria'),
             Field('medida_basica_seq_secundaria', css_class='medida_basica_seq_secundaria'),
             Field('disciplinas_seq_secundaria', css_class='disciplinas_seq_secundaria'),
+            HTML('<p class="form-text">Uma ou mais devem ser preenchidas no caso de estar quantificando o backbone'
+                 ' principal. Caso esteja sendo especificado somente o backbone secundário, deve ser especificado somente'
+                 ' uma SEQ secundária</p>'),
         )
 
 
--- fibra/quantificadores.py
+++ fibra/quantificadores.py
@@ -64,12 +64,22 @@ def get_xls(df: pandas.DataFrame) -> bytes:
 def _calcular_quantificacao_total_projeto_simples(entrada):
     sala_equipamento = entrada.seq.quantificacao_equipamentos_de_fibra_seq
     sets = entrada.seq.quantificacao_equipamentos_de_fibra_sets
-    quantificacao_total = {
-        "Chassi DIO (Distribuido Interno Optico) com 24 portas - 1U - 19": sala_equipamento.dios + sets.dios,
-        "Bandeja para emenda de fibra no DIO - (comporta ate 12 emendas)": sala_equipamento.caixas_de_emenda + sets.caixas_de_emenda,
-        "Terminador Optico para 8 fibras": sala_equipamento.terminadores_opticos + sets.terminadores_opticos}
+    quantificacao_total = {}
 
-    if entrada.caracteristicas_set.nucleo == entrada.caracteristicas_seq_secundaria.nucleo:
+    dict_fibras = entrada.seq.quantificacao_backbones_de_segundo_nivel_por_disciplina
+    for fibras in dict_fibras:
+        quantificacao_total[
+            f"Cabo de Fibra Optica {entrada.caracteristicas_set.categoria} FO{entrada.caracteristicas_set.modo}{entrada.caracteristicas_set.indice} {entrada.caracteristicas_set.nucleo} x 125um - com {fibras * 2} fibras"] = \
+        dict_fibras[fibras]
+
+    quantificacao_total[
+        "Chassi DIO (Distribuido Interno Optico) com 24 portas - 1U - 19"] = sala_equipamento.dios + sets.dios
+    quantificacao_total[
+        "Bandeja para emenda de fibra no DIO - (comporta ate 12 emendas)"] = sala_equipamento.caixas_de_emenda + sets.caixas_de_emenda
+    quantificacao_total[
+        "Terminador Optico para 8 fibras"] = sala_equipamento.terminadores_opticos + sets.terminadores_opticos
+
+    if entrada.caracteristicas_set == entrada.caracteristicas_seq_secundaria:
         quantificacao_total[
             f"Acoplador optico {entrada.caracteristicas_set.nucleo} x 125um - {entrada.caracteristicas_set.modo} - LC - duplo"] = sala_equipamento.acopladores_lc_duplo + sets.acopladores_lc_duplo
 
@@ -107,7 +117,53 @@ def _calcular_quantificacao_total_projeto_simples(entrada):
 
 
 def _calcular_quantificacao_total_projeto_complexo(entrada):
-    return _calcular_quantificacao_total_projeto_simples(entrada)
+    seqp = entrada.seq.quantificacao_equipamentos_de_fibra_seq_primaria
+    seqs = entrada.seq.quantificacao_equipamentos_de_fibra_seqs_secundarias
+    print(seqp)
+    quantificacao_total = {}
+
+    quantificacao_total[
+        "Chassi DIO (Distribuido Interno Optico) com 24 portas - 1U - 19"] = seqp.dios + seqs.dios
+    quantificacao_total[
+        "Bandeja para emenda de fibra no DIO - (comporta ate 12 emendas)"] = seqp.caixas_de_emenda + seqs.caixas_de_emenda
+    quantificacao_total[
+        "Terminador Optico para 8 fibras"] = seqp.terminadores_opticos + seqs.terminadores_opticos
+
+    if entrada.caracteristicas_set.nucleo == entrada.caracteristicas_seq_secundaria.nucleo:
+        quantificacao_total[
+            f"Acoplador optico {entrada.caracteristicas_set.nucleo} x 125um - {entrada.caracteristicas_set.modo} - LC - duplo"] = seqp.acopladores_lc_duplo + seqs.acopladores_lc_duplo
+
+        quantificacao_total[
+            f"Cordao Optico {entrada.caracteristicas_set.nucleo} x 125um - {entrada.caracteristicas_set.modo} - 3m - duplo - conector LC"] = seqp.cordao_optico + seqs.cordao_optico
+
+        quantificacao_total[
+            f"Pig tail {entrada.caracteristicas_set.nucleo} x 125um - {entrada.caracteristicas_set.modo} - 1,5m - simples - conector LC"] = seqp.pig_tails_simples + seqs.pig_tails_simples
+
+        quantificacao_total[
+            f"Pig tail {entrada.caracteristicas_set.nucleo} x 125um - {entrada.caracteristicas_set.modo} - 3,0m - duplo - conector LC"] = seqp.pig_tails_duplos + seqs.pig_tails_duplos
+
+    else:
+        quantificacao_total[
+            f"Acoplador optico {entrada.caracteristicas_set.nucleo} x 125um - {entrada.caracteristicas_set.modo} - LC - duplo"] = seqs.acopladores_lc_duplo
+        quantificacao_total[
+            f"Acoplador optico {entrada.caracteristicas_seq_secundaria.nucleo} x 125um - {entrada.caracteristicas_seq_secundaria.modo} - LC - duplo"] = seqp.acopladores_lc_duplo
+
+        quantificacao_total[
+            f"Cordao Optico {entrada.caracteristicas_set.nucleo} x 125um - {entrada.caracteristicas_set.modo} - 3m - duplo - conector LC"] = seqs.cordao_optico
+        quantificacao_total[
+            f"Cordao Optico {entrada.caracteristicas_seq_secundaria.nucleo} x 125um - {entrada.caracteristicas_seq_secundaria.modo} - 3m - duplo - conector LC"] = seqp.cordao_optico
+
+        quantificacao_total[
+            f"Pig tail {entrada.caracteristicas_set.nucleo} x 125um - {entrada.caracteristicas_set.modo} - 1,5m - simples - conector LC"] = seqs.pig_tails_simples
+        quantificacao_total[
+            f"Pig tail {entrada.caracteristicas_seq_secundaria.nucleo} x 125um - {entrada.caracteristicas_seq_secundaria.modo} - 1,5m - simples - conector LC"] = seqp.pig_tails_simples
+
+        quantificacao_total[
+            f"Pig tail {entrada.caracteristicas_set.nucleo} x 125um - {entrada.caracteristicas_set.modo} - 3,0m - duplo - conector LC"] = seqs.pig_tails_duplos
+        quantificacao_total[
+            f"Pig tail {entrada.caracteristicas_seq_secundaria.nucleo} x 125um - {entrada.caracteristicas_seq_secundaria.modo} - 3,0m - duplo - conector LC"] = seqp.pig_tails_duplos
+
+    return quantificacao_total
 
 
 def _calcular_quantificacao_projeto(
@@ -115,10 +171,9 @@ def _calcular_quantificacao_projeto(
 ) -> dict[str, dict[str, str]]:
     quantificacao_fibra = {}
     if entrada.backbone_primario:
-        seqp = entrada.seq.quantificacao_equipamentos_de_fibra_seq_primaria
-        seqs = entrada.seq.quantificacao_equipamentos_de_fibra_seqs_secundarias
-        quantificacao_fibra["Quantificacao total"]
-        _calcular_quantificacao_total_projeto_complexo(entrada)
+        quantificacao_fibra[
+            "Quantificacao total"] = _calcular_quantificacao_total_projeto_complexo(
+            entrada)
     else:
         quantificacao_fibra[
             "Quantificacao total"] = _calcular_quantificacao_total_projeto_simples(
@@ -128,25 +183,32 @@ def _calcular_quantificacao_projeto(
 
 
 if __name__ == "__main__":
-    carac_fibra1 = CaracteristicasFibra("SM", "9", "Indice Gradual", "Loose")
-    carac_fibra2 = CaracteristicasFibra("SM", "9", "Indice Degrau", "Loose")
-    carac_fibra3 = CaracteristicasFibra("MM", "50", "Indice Degrau", "Loose")
-    sala_telecom1 = SET(2, [4])
-    sala_telecom2 = SET(3, [4])
-    sala_telecom3 = SET(4, [4])
+    carac_fibra1 = CaracteristicasFibra("SM", "9", "IG", "Loose")
+    carac_fibra2 = CaracteristicasFibra("SM", "9", "ID", "Loose")
+    carac_fibra3 = CaracteristicasFibra("MM", "50", "ID", "Loose")
+    sala_telecom1 = SET(2, [4, 2])
+    sala_telecom2 = SET(3, [4, 2])
+    sala_telecom3 = SET(4, [4, 2])
 
     sala_equipamento1 = SEQSecundaria(4, 1, 5,
                                       [sala_telecom1, sala_telecom2, sala_telecom3])
-    sala_equipamento2 = SEQSecundaria(12, 1, 5,
+    sala_equipamento2 = SEQSecundaria(4, 1, 5,
+                                      [sala_telecom1, sala_telecom2, sala_telecom3])
+    sala_equipamento3 = SEQSecundaria(4, 1, 5,
+                                      [sala_telecom1, sala_telecom2, sala_telecom3])
+    sala_equipamento4 = SEQSecundaria(4, 1, 5,
                                       [sala_telecom1, sala_telecom2, sala_telecom3])
 
-    sala_equipamento_mestre = SEQPrimaria(12, 5, [sala_equipamento1, sala_equipamento2])
+    sala_equipamento_mestre = SEQPrimaria(8, 5, [sala_equipamento1, sala_equipamento2])
 
     input = CleanedInput(sala_equipamento1, carac_fibra3, carac_fibra2, carac_fibra3,
                          False, True)
+
     # input = CleanedInput(sala_equipamento_mestre, carac_fibra3, carac_fibra2,  None, True)
+    # print(sala_equipamento_mestre.quantificacao_equipamentos_de_fibra_seq_primaria)
+    # print(sala_equipamento_mestre.quantificacao_equipamentos_de_fibra_seqs_secundarias)
 
     dicionario = _calcular_quantificacao_projeto(input)
     for i in dicionario['Quantificacao total']:
-        if not dicionario['Quantificacao total'][i]:
+        if dicionario['Quantificacao total'][i] != 0:
             print(f"{i} : {dicionario['Quantificacao total'][i]}")
