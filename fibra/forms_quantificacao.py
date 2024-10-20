e5cde34bb81ad40f8ecbe7e50b20f68a128feeb5
Eduardo Costa de Souza <eduardbh2014@gmail.com>
2024-10-20 16:45:40 -0300

    Melhoria na interface

--- fibra/forms_quantificacao.py
+++ fibra/forms_quantificacao.py
@@ -2,6 +2,8 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, HTML
 
+from fibra.quantificadores import Quantificacao
+
 MODOS_FIBRA = [
     ('', 'Não selecionado'),
     ('MM', 'Multimodo'),
@@ -28,76 +30,151 @@ NUCLEOS_FIBRA = [
     ('62.5', '62.5µm'),
 ]
 
+FIBRA_INTERFACE = [
+    ('', 'Não selecionado'),
+    ('MM50', 'Multimodo 50µm'),
+    ('MM62.5', 'Multimodo 62.5µm'),
+    ('SM9', 'Monomodo 9µm'),
+]
+
+TIPO_QUANTIFICACAO_INTERFACE = [
+    ('CAMPUS', 'Campus'),
+    ('PREDIO', 'Prédio'),
+]
+
+
+class FormChoicesInterface(forms.Form):
+    tipo_quantificacao = forms.ChoiceField(
+        choices=TIPO_QUANTIFICACAO_INTERFACE,
+        widget=forms.RadioSelect,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            HTML('<h2>Tipo de quantificação</h2>'),
+            Field('tipo_quantificacao'),
+        )
+
+
+class FormConfigCampusInterface(forms.Form):
+    modo_fibra_seq_primaria = forms.ChoiceField(
+        label="Fibra na SEQ primária",
+        choices=FIBRA_INTERFACE,
+    )
+    modo_fibra_seq_secundaria = forms.ChoiceField(
+        label="Fibra nas SEQs secundárias",
+        choices=FIBRA_INTERFACE,
+    )
+    modo_fibra_set = forms.ChoiceField(
+        label="Fibra nas SETs",
+        choices=FIBRA_INTERFACE,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            HTML('<h2>Tipos de fibra</h2>'),
+            Field('modo_fibra_seq_primaria'),
+            Field('modo_fibra_seq_secundaria'),
+            Field('modo_fibra_set'),
+        )
+
+
+class FormConfigPredioInterface(forms.Form):
+    modo_fibra_seq_primaria = forms.ChoiceField(
+        label="Fibra na SEQ",
+        choices=FIBRA_INTERFACE,
+    )
+    modo_fibra_set = forms.ChoiceField(
+        label="Fibra nas SETs",
+        choices=FIBRA_INTERFACE,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            HTML('<h2>Tipos de fibra</h2>'),
+            Field('modo_fibra_seq_primaria'),
+            Field('modo_fibra_set'),
+        )
+
 
 class FormConfig(forms.Form):
     backbone_primario = forms.BooleanField(
         label="Quantificar Backbone Primário",
         required=False,
-        help_text='Implica em quantificar SEQ primária e secundária'
+        help_text='Implica em quantificar SEQ primária e secundária',
     )
     backbone_secundario = forms.BooleanField(
         label="Backbone Secundário",
         required=False,
-        help_text='Implica em quantificar SEQ secundária e SETs'
+        help_text='Implica em quantificar SEQ secundária e SETs',
     )
 
     modo_fibra_seq_primaria = forms.ChoiceField(
         label="Modo da Fibra na SEQ primária",
         choices=MODOS_FIBRA,
-        required=False
+        required=False,
     )
     nucleo_fibra_seq_primaria = forms.ChoiceField(
         label="Núcleo da Fibra na SEQ primária",
         choices=NUCLEOS_FIBRA,
-        required=False
+        required=False,
     )
     indice_fibra_seq_primaria = forms.ChoiceField(
         label="Índice da Fibra na SEQ primária",
         choices=INDICES_FIBRA,
-        required=False
+        required=False,
     )
     categoria_fibra_seq_primaria = forms.ChoiceField(
         label="Categoria da Fibra na SEQ primária",
         choices=CATEGORIAS_FIBRA,
-        required=False
+        required=False,
     )
 
     modo_fibra_seq_secundaria = forms.ChoiceField(
         label="Modo da Fibra nas SEQs secundárias",
-        choices=MODOS_FIBRA
+        choices=MODOS_FIBRA,
     )
     nucleo_fibra_seq_secundaria = forms.ChoiceField(
         label="Núcleo da Fibra nas SEQs secundárias",
-        choices=NUCLEOS_FIBRA
+        choices=NUCLEOS_FIBRA,
     )
     indice_fibra_seq_secundaria = forms.ChoiceField(
         label="Índice da Fibra nas SEQs secundárias",
-        choices=INDICES_FIBRA
+        choices=INDICES_FIBRA,
     )
     categoria_fibra_seq_secundaria = forms.ChoiceField(
         label="Categoria da Fibra nas SEQs secundárias",
-        choices=CATEGORIAS_FIBRA
+        choices=CATEGORIAS_FIBRA,
     )
 
     modo_fibra_set = forms.ChoiceField(
         label="Modo da Fibra nas SETs",
         choices=MODOS_FIBRA,
-        required=False
+        required=False,
     )
     nucleo_fibra_set = forms.ChoiceField(
         label="Núcleo da Fibra nas SETs",
         choices=NUCLEOS_FIBRA,
-        required=False
+        required=False,
     )
     indice_fibra_set = forms.ChoiceField(
         label="Índice da Fibra nas SETs",
         choices=INDICES_FIBRA,
-        required=False
+        required=False,
     )
     categoria_fibra_set = forms.ChoiceField(
         label="Categoria da Fibra nas SETs",
         choices=CATEGORIAS_FIBRA,
-        required=False
+        required=False,
     )
 
     def __init__(self, *args, **kwargs):
@@ -141,12 +218,10 @@ class FormConfig(forms.Form):
 class FormSEQPrimaria(forms.Form):
     medida_basica_seq_primaria = forms.FloatField(
         label="Distância média das SEQs secundárias à SEQ primária",
-        required=False,
         widget=forms.NumberInput(attrs={'min': '0', 'step': '0.1'})
     )
     disciplinas_seq_primaria = forms.IntegerField(
-        label="Disciplinas chegando na SEQ primária",
-        required=False,
+        label="Disciplinas chegando à SEQ primária",
         widget=forms.NumberInput(attrs={'min': '1', 'step': '1'})
     )
 
@@ -155,10 +230,8 @@ class FormSEQPrimaria(forms.Form):
         self.helper = FormHelper()
         self.helper.form_tag = False
         self.helper.layout = Layout(
-            HTML('<h2>Dados SEQ primária</h2>'),
             Field('medida_basica_seq_primaria'),
             Field('disciplinas_seq_primaria'),
-            HTML('<p class="form-text">Só devem ser preenchido no caso de estar especificando o backbone principal</p>'),
         )
 
 
@@ -167,11 +240,11 @@ class FormSEQSecundaria(forms.Form):
         label="Andar"
     )
     medida_basica_seq_secundaria = forms.FloatField(
-        label="Pé direito SEQ secundária",
+        label="Pé direito",
         widget=forms.NumberInput(attrs={'min': '0', 'step': '0.1'})
     )
     disciplinas_seq_secundaria = forms.IntegerField(
-        label="Disciplinas chegando na SEQ secundária",
+        label="Disciplinas chegando na SEQ",
         widget=forms.NumberInput(attrs={'min': '1', 'step': '1'})
     )
 
@@ -183,9 +256,6 @@ class FormSEQSecundaria(forms.Form):
             Field('andar', css_class='andar_seq_secundaria'),
             Field('medida_basica_seq_secundaria', css_class='medida_basica_seq_secundaria'),
             Field('disciplinas_seq_secundaria', css_class='disciplinas_seq_secundaria'),
-            HTML('<p class="form-text">Uma ou mais devem ser preenchidas no caso de estar quantificando o backbone'
-                 ' principal. Caso esteja sendo especificado somente o backbone secundário, deve ser especificado somente'
-                 ' uma SEQ secundária</p>'),
         )
 
 
